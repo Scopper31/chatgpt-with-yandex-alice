b67c6447fcdec28f38d128c519a7ca8f64e41c50
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-01-28 23:18:15 +0300

    Create site.py

---
+++ site.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+import logging
+import json
+import os
+import os
+import openai
+
+key = "your chat gpt api"
+openai.api_key = key
+
+
+def ans(s):
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=s,
+      temperature=0.5,
+      max_tokens=1000,
+      top_p=1.0,
+      frequency_penalty=0.5,
+      presence_penalty=0.0,
+    )
+    return response["choices"][0]["text"]
+
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/", methods=["POST"])
+def main():
+    logging.info(request.json)
+    response = {
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "GO"
+    else:
+        response["response"]["text"] = ans(req["request"]["original_utterance"])
+
+    return json.dumps(response)
